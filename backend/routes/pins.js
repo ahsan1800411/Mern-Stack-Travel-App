d7fe609785cbf9b54467d212d6be9e5e8420e0f2
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-07-26 07:52:00 +0500

    Create a route to get all the pins

--- backend/routes/pins.js
+++ backend/routes/pins.js
@@ -1,12 +1,22 @@
 const router = require("express").Router();
 const Pin = require("../models/Pin");
 
-// create a new pin ==> post request ==>/api/pins
-router.post("/", async (req, res) => {
+// create a new pin ==> post request ==>/api/pins/create
+router.post("/create", async (req, res) => {
   const newPin = new Pin(req.body);
   try {
     const savedPin = await newPin.save();
     res.status(200).json(savedPin);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+// get all the pins ==> get request ==>/api/pins
+router.get("/", async (req, res) => {
+  const pins = await Pin.find();
+  try {
+    res.status(200).json(pins);
   } catch (error) {
     res.status(400).send(error.message);
   }
