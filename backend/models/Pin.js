96faf5d528c44bd14a83d329bafd79adeb17ebf0
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-07-26 07:28:52 +0500

    creating the models

---
+++ backend/models/Pin.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const PinSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+    long: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Pin", PinSchema);
