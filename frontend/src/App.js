a952b51be9d1bf3f3a9ac75b33db20ecc034172a
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-07-26 12:08:01 +0500

    Added the basic CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,14 +38,14 @@ function App() {
           <label>Place</label>
           <h4 className='place'>Eiffel Tower</h4>
           <label>Review</label>
-          <p>Beautiful Place, I like the most</p>
+          <p className='desc'> Beautiful Place, I like the most</p>
           <label>Rating</label>
           <div className='stars'>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
+            <Star className='star' />
+            <Star className='star' />
+            <Star className='star' />
+            <Star className='star' />
+            <Star className='star' />
           </div>
           <label>Information</label>
           <span className='username'>
