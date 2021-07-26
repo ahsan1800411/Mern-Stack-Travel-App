e6d2608334c1f158a578e90ea51f3e67f9f07407
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-07-26 07:18:31 +0500

    Connect with the Database

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,19 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB Database");
+  } catch (error) {
+    console.error(error);
+  }
+};
+connectDB();
+
 const port = process.env.PORT || 8800;
 
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
