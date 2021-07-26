f47b9ddfbb1a06dd31feb56f830cc86b322a0fa3
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-07-26 09:29:03 +0500

    Install the map package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
 function App() {
-  return <div>Helllo World</div>;
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    />
+  );
 }
 
 export default App;
