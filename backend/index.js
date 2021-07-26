36c69282dcf551e6ca0aa3ebd57662284c3af333
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-07-26 08:06:57 +0500

    added some options

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
 const pinRoutes = require("./routes/pins");
+const userRoutes = require("./routes/users");
 
 app.use(express.json());
 
@@ -11,6 +12,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
     console.log("Connected to mongoDB Database");
   } catch (error) {
@@ -21,6 +23,7 @@ connectDB();
 
 // Routes
 app.use("/api/pins", pinRoutes);
+app.use("/api/users", userRoutes);
 
 const port = process.env.PORT || 8800;
 
